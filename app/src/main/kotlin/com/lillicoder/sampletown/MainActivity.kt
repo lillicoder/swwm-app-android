ffdefff974ff65066fd9a151b74c5b3c88e4bd50
lillicoder <scott@lillicoder.com>
2020-12-22 20:11:16 -0500

    Created navigation drawer demo

--- app/src/main/kotlin/com/lillicoder/sampletown/MainActivity.kt
+++ app/src/main/kotlin/com/lillicoder/sampletown/MainActivity.kt
@@ -15,11 +15,18 @@
  */
 package com.lillicoder.sampletown
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +34,39 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        mDrawerToggle = configureDrawer(toolbar, drawerLayout)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        mDrawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        mDrawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    /**
+     * Configures the given drawer layout for this activity.
+     * @param toolbar [Toolbar] for this activity.
+     * @param drawer [DrawerLayout] to configure.
+     */
+    private fun configureDrawer(toolbar: Toolbar, drawer: DrawerLayout): ActionBarDrawerToggle {
+        val toggle =
+            ActionBarDrawerToggle(this,
+                                  drawer,
+                                  toolbar,
+                                  R.string.content_desc_drawer_open,
+                                  R.string.content_desc_drawer_close)
+
+        drawer.addDrawerListener(toggle)
+        toggle.isDrawerIndicatorEnabled = true
+        toggle.syncState()
+
+        return toggle
     }
 
 }
