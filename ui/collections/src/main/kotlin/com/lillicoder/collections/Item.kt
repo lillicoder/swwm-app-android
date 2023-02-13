5d728ba7ada8329295d1b4b4c3e95447ebb87dab
lillicoder <scott@lillicoder.com>
2023-02-13 13:26:44 -0500

    Adjusted Collections module to have more appropriate namespace

--- ui/collections/src/main/kotlin/com/lillicoder/collections/Item.kt
+++
@@ -1,9 +0,0 @@
-package com.lillicoder.collections
-
-import androidx.annotation.DrawableRes
-
-/**
- * Represents an arbitrary item.
- * @param imageResourceId Image resource ID.
- */
-data class Item(@DrawableRes val imageResourceId: Int)
