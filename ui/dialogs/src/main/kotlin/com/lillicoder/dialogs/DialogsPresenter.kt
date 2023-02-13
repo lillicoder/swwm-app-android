c3172296f2163381bf968929949fe45c9244e370
lillicoder <scott@lillicoder.com>
2023-02-13 13:44:35 -0500

    Adjusted dialogs module to have more appropriate namespace

--- ui/dialogs/src/main/kotlin/com/lillicoder/dialogs/DialogsPresenter.kt
+++
@@ -1,8 +0,0 @@
-package com.lillicoder.dialogs
-
-/**
- * [DialogsContract.Presenter] for handling events from a [DialogsContract.View].
- */
-class DialogsPresenter(
-    private val view: DialogsContract.View
-) : DialogsContract.Presenter
