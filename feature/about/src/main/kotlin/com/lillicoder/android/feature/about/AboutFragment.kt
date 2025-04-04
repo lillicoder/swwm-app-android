a24cade56838bb0324f0b37d14664e489a2e03ca
lillicoder <scott@lillicoder.com>
2025-04-04 13:34:48 -0400

    Removed unused imports

--- feature/about/src/main/kotlin/com/lillicoder/android/feature/about/AboutFragment.kt
+++ feature/about/src/main/kotlin/com/lillicoder/android/feature/about/AboutFragment.kt
@@ -21,9 +21,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
